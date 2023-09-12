4d2dcd4ebd13c91d11b4130450f78576b5b0e090
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-09-12 09:33:27 -0700

    d

--- breadmacro.ino
+++ breadmacro.ino
@@ -1,3 +1,4 @@
+
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Wire.h>
@@ -17,13 +18,14 @@ int buttonRState = 0;
 // Variable to store the time when last event happened
 unsigned long lastEvent1 = 0;
 unsigned long lastEvent2 = 0;
-void setup() {
+void setup(void) {
+
   Serial.begin(115200);
   pinMode(buttonRPin, INPUT);
   pinMode(sensorPin1, INPUT);
   pinMode(sensorPin2, INPUT);
   pinMode(buttonLPin, INPUT);
-    pinMode(exitpin, INPUT);
+  pinMode(exitpin, INPUT);
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
     Serial.println(F("SSD1306 allocation failed"));
     for (;;)
@@ -41,53 +43,76 @@ void setup() {
   delay(100);
 }
 
-void loop() {
-C1:
-  if (digitalRead(sensorPin1) == LOW) {
-    display.println("SOUND [0x7]. ");
-    display.display();
-    // If 25ms have passed since last LOW state, it means that
-    // the clap is detected and not due to any spurious sounds
-    display.stopscroll();
-    // Remember when last event happened
-    lastEvent1 = millis();
-  }
-  if (digitalRead(sensorPin2) == LOW) {
-    display.println("SOUND [0x8].");
-    display.display();
-    // If 25ms have passed since last LOW state, it means that
-    // the clap is detected and not due to any spurious sounds
-    display.stopscroll();
-    // Remember when last event happened
-    lastEvent2 = millis();
+void loop(void) {
+  while (true) {
+    int soundValue = 0; // create variable to store many different readings
+    for (int i = 0; i < 32; i++) // create a for loop to read
+    {
+      soundValue += analogRead(sensorPin2);
+      soundValue += analogRead(sensorPin1);
+    } // read the sound sensor
 
-  }
-  if (digitalRead(buttonRPin) == HIGH && digitalRead(buttonLPin) != HIGH) {
-    display.println("<0x3C> RIGHT.");
-    display.display();
-    delay(500);
-    display.startscrollright(0x00, 0x0F);
-    delay(1000);
-    display.stopscroll();
-  }
-  if (digitalRead(buttonRPin) != HIGH && digitalRead(buttonLPin) == HIGH) {
-    display.println("<0x3C>  LEFT.");
-    display.display();
-    delay(500);
-    display.startscrollleft(0x00, 0x0F);
-    delay(1000);
-    display.stopscroll();
-  }
+    soundValue >>= 10; // bitshift operation
+    // if a value higher than 500 is registered, we will print the following
+    // this is done so that we can clearly see if the threshold is met
+    if (soundValue > 500) {
+      display.println("         ||        ");
+      display.println("       ||||||      ");
+      display.println("     |||||||||     ");
+      display.println("   |||||||||||||   ");
+      display.println(" ||||||||||||||||| ");
+      display.println("   |||||||||||||   ");
+      display.println("     |||||||||     ");
+      display.println("       ||||||      ");
+      display.println("         ||        ");
+      display.print("DECIBELS: ");
+      display.println(soundValue); // print the value of sound sensor
+    }
 
-  // If pin goes LOW, sound is detected
- 
-  if(digitalRead(exitpin)==HIGH){
-  display.setCursor(0, 0);
-  // Display static text
-   display.clearDisplay();
-  display.println("<0xf> WAITS.");
-  display.display();
-  delay(100);
-  goto C1;
+    if (digitalRead(sensorPin1) == LOW) {
+      display.println("SOUND [0x7]. ");
+      display.display();
+      // If 25ms have passed since last LOW state, it means that
+      // the clap is detected and not due to any spurious sounds
+      display.stopscroll();
+      // Remember when last event happened
+      lastEvent1 = millis();
+    }
+    if (digitalRead(sensorPin2) == LOW) {
+      display.println("SOUND [0x8].");
+      display.display();
+      // If 25ms have passed since last LOW state, it means that
+      // the clap is detected and not due to any spurious sounds
+      display.stopscroll();
+      // Remember when last event happened
+      lastEvent2 = millis();
+    }
+    if (digitalRead(buttonRPin) == HIGH && digitalRead(buttonLPin) != HIGH) {
+      display.println("<0x3C> RIGHT.");
+      display.display();
+      delay(500);
+      display.startscrollright(0x00, 0x0F);
+      delay(1000);
+      display.stopscroll();
+    }
+    if (digitalRead(buttonRPin) != HIGH && digitalRead(buttonLPin) == HIGH) {
+      display.println("<0x3C>  LEFT.");
+      display.display();
+      delay(500);
+      display.startscrollleft(0x00, 0x0F);
+      delay(1000);
+      display.stopscroll();
+    }
+
+    // If pin goes LOW, sound is detected
+
+    if (digitalRead(exitpin) == HIGH) {
+      display.setCursor(0, 0);
+      // Display static text
+      display.clearDisplay();
+      display.println("<0xf> WAITS.");
+      display.display();
+      delay(100);
+    }
   }
 }
